9bd9b4df3d2302ca1851fa78f9ad9821e440ca88
Daniel Ukaji <dannyukaji@gmail.com>
2022-06-20 05:38:42 -0700

    Added Database Tables and Migrations. Implemented Database Section

--- PurchaseRequest.Web/Data/Migrations/20220620121248_AddedLeaveTables.Designer.cs
+++ PurchaseRequest.Web/Data/Migrations/20220620121248_AddedLeaveTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PurchaseRequest.Web.Data;
 
@@ -11,9 +12,10 @@ using PurchaseRequest.Web.Data;
 namespace PurchaseRequest.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220620121248_AddedLeaveTables")]
+    partial class AddedLeaveTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
