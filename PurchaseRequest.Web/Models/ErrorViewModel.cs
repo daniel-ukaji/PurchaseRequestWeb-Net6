7fd76e67c3ba2c8b62b84a90e26d1c3ac2f94fcd
LBS <LBS@LBS-ICT-VM001>
2022-06-20 01:53:19 -0700

    Add project files.

---
+++ PurchaseRequest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PurchaseRequest.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
