55df9eb507e7fcb1e8acdeef6d58eecb5edcce11
Daniel Ukaji <dannyukaji@gmail.com>
2022-06-21 08:14:21 -0700

    Modified the crud screens and added datatables and swal.

---
+++ PurchaseRequest.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PurchaseRequest.Web.Data;
+using PurchaseRequest.Web.Models;
+
+namespace PurchaseRequest.Web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
